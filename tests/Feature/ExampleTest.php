fe53dcc365254ff80899edb1b693e772d10fb674
jechaviz <jesus.cgalaviz@gmail.com>
2025-05-23 15:12:00 -0600

    Update ExampleTest

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -7,6 +7,7 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class ExampleTest extends TestCase
 {
+    use RefreshDatabase;
     /**
      * A basic test example.
      *
@@ -17,5 +18,6 @@ class ExampleTest extends TestCase
         $response = $this->get('/');
 
         $response->assertStatus(200);
+        $response->assertSee('Buyniverse');
     }
 }
